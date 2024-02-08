462587bbe2ca6e7bf933b090cce413ada64a22c0
SeiPanda <70478113+SeiPanda@users.noreply.github.com>
2024-02-08 11:47:01 +0100

    add filter button

--- src/app/shared/components/filter/filter.component.ts
+++ src/app/shared/components/filter/filter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../../../core/models/recipe.model";
+import {RecipesDummy} from "../../../core/models/recipes.dummy";
+import {FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'app-filter',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent {
+  toggle: boolean = false;
+  recipes: Recipe[] = RecipesDummy;
+  filter_Food = this._formBuilder.group({
+    fleisch: false,
+    fisch: false,
+    vegetarisch: false,
+    vegan: false,
+  });
 
+  filter_Aufwand = this._formBuilder.group({
+    high: false,
+    middle: false,
+    low: false,
+  });
+
+  filter_Main = this._formBuilder.group({
+    pasta: false,
+    kartoffeln: false,
+    reis: false,
+    gemüse: false,
+  });
+
+  constructor(private _formBuilder: FormBuilder) {}
+  toggleFilterArea(){
+    this.toggle = !this.toggle;
+  }
 }
--- src/app/shared/components/filter/filter.module.ts
+++ src/app/shared/components/filter/filter.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {MaterialModule} from "../../material.module";
 import { CommonModule } from "@angular/common";
 import {FilterComponent} from "./filter.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     MaterialModule,
     CommonModule,
+    ReactiveFormsModule,
   ],
   declarations: [FilterComponent],
   exports: [FilterComponent]
