a45c628cf6222df5e044a139438a5872ab212dca
SeiPanda <70478113+SeiPanda@users.noreply.github.com>
2024-04-04 21:38:30 +0200

    add dropdown design, delete icon

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent implements OnInit{
 
   openDialog(): void {
     this.dialog.open(GroceryListDialogComponent, {
-      width: '500px',
+      width: '400px',
     });
   }
 
