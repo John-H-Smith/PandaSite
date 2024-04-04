33a60332a4d34b0f346ffb0676577ba504794175
Michelle Sandner <michelle.sandner@tecvia.com>
2024-04-04 15:13:00 +0200

    fix ripple effect

--- src/app/pages/recipes/grocery-list-dialog/ingredient-list-item/ingredient-list-item.component.ts
+++ src/app/pages/recipes/grocery-list-dialog/ingredient-list-item/ingredient-list-item.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-interface Food {
-  value: string;
-  viewValue: string;
-}
+
 @Component({
   selector: 'app-ingredient-list-item',
   templateUrl: './ingredient-list-item.component.html',
   styleUrls: ['./ingredient-list-item.component.scss']
 })
 export class IngredientListItemComponent {
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'g'},
-    {value: 'pizza-1', viewValue: 'kg'},
-    {value: 'tacos-2', viewValue: 'ml'},
-  ];
-
-  selected = this.foods[0];
+  selected = 'option2';
 
 }
