7a2b002f3537713defbc2f5efbb0cdca8749964b
Michelle Sandner <michelle.sandner@tecvia.com>
2024-04-05 14:21:30 +0200

    add create item, formgroup

---
+++ src/app/pages/recipes/grocery-list-dialog/create-ingredient-list-item/create-ingredient-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create-ingredient-list-item',
+  templateUrl: './create-ingredient-list-item.component.html',
+  styleUrls: ['./create-ingredient-list-item.component.scss']
+})
+export class CreateIngredientListItemComponent {
+  selected = '';
+  formGroup = new FormGroup({
+    name: new FormControl<string>('', [Validators.required]),
+    amount: new FormControl<string>('', [Validators.required]),
+    unit: new FormControl<string>('', [Validators.required])
+
+  })
+  addIngredient(){
+    if(this.formGroup.invalid) {
+      console.log("invalid")
+    }
+  }
+}
