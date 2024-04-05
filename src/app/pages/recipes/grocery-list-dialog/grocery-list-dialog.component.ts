fdcb0a6a979f1727e6dd2f1098997823d37c8230
Michelle Sandner <michelle.sandner@tecvia.com>
2024-04-05 13:31:28 +0200

    add list desgin

--- src/app/pages/recipes/grocery-list-dialog/grocery-list-dialog.component.ts
+++ src/app/pages/recipes/grocery-list-dialog/grocery-list-dialog.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {MAT_TOOLTIP_DEFAULT_OPTIONS} from "@angular/material/tooltip";
+import {myCustomTooltipDefaults} from "../../../shared/components/tooltip/tooltip.component";
 
 @Component({
   selector: 'app-grocery-list-dialog',
   templateUrl: './grocery-list-dialog.component.html',
-  styleUrls: ['./grocery-list-dialog.component.scss']
+  styleUrls: ['./grocery-list-dialog.component.scss'],
+  providers: [{provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults}],
 })
 export class GroceryListDialogComponent {
   constructor(public dialogRef: MatDialogRef<GroceryListDialogComponent>) {}
